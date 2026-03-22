926e7278c53727d324701681fe62191842e429c0
Matt-Wallbank <m.wallbank@my.bpp.com>
2026-03-22 15:23:14 +0000

    Python set up

---
+++ Topic 4_structures.py
@@ -0,0 +1,23 @@
+# Lists
+print('Lists:')
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
+print(fruits[1])  # Access by index
+
+# Sets
+print('Sets:')
+unique_numbers = {1, 2, 3, 3, 2}
+unique_numbers.add(4)
+print(unique_numbers)  # Duplicates removed automatically
+
+# Dictionaries
+print('Dictionaries:')
+student = {"name": "Sam", "grade": "A", "module": "Advanced Programming"}
+print(student["module"])
+student["grade"] = "A+"
+
+# Tuples
+print('Tuples:')
+coordinates = (10, 20)
+print(coordinates)
